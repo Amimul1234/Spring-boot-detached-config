2644c58f3977071d6ed265ae0f22c9a0c5495cbc
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-19 23:49:14 +0600

    Learned @Value annotation features

---
+++ src/main/java/com/example/springbootexternalizeconfig/GreetingController.java
@@ -0,0 +1,38 @@
+package com.example.springbootexternalizeconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author Amimul Ehsan
+ * @Created at 10/19/21
+ * @Project Spring boot externalize config
+ */
+
+@RestController
+public class GreetingController {
+
+    //Default value given as a fallback mechanism
+    @Value("${my.greeting : default value}")
+    private String greetingMessage;
+
+    @Value("some static message")
+    private String staticMessage;
+
+    //List of values
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    //Key value pairs
+    @Value("#{${dbValues}}")
+    private Map<String, String> dbValues;
+
+    @GetMapping("greeting")
+    public String greeting() {
+        return greetingMessage + staticMessage + listValues + dbValues;
+    }
+}
