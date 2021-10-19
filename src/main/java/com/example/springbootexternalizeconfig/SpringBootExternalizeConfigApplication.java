4e0a1fdc4d1ebe8b3dee6395e0545d02af14a0c5
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-19 23:03:01 +0600

    First commit

---
+++ src/main/java/com/example/springbootexternalizeconfig/SpringBootExternalizeConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexternalizeconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExternalizeConfigApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(SpringBootExternalizeConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootexternalizeconfig/SpringBootExternalizeConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootexternalizeconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExternalizeConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
